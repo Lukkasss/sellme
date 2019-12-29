e4d8c98a06d4df9fa87544486e920a15ef01f785
Lucas Vilela <f.lucasgv@gmail.com>
2019-12-29 01:14:31 -0300

    project created

---
+++ src/test/java/com/lucasgv/sellme/SellmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgv.sellme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
