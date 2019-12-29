feb2665af858d09b1851660df33bee12070db8d3
Lucas Vilela <f.lucasgv@gmail.com>
2019-12-29 01:59:08 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/lucasgv/sellme/controller/CategoriaController.java
+++ src/main/java/com/lucasgv/sellme/controller/CategoriaController.java
@@ -1,15 +1,27 @@
 package com.lucasgv.sellme.controller;
 
+import com.lucasgv.sellme.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categoria")
 public class CategoriaController {
 
     @GetMapping
-    public String listar(){
-        return "REST funcionando!";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1L, "Informática");
+        Categoria cat2 = new Categoria(2L, "Escritório");
+
+        List<Categoria> listaCategoria = new ArrayList<>();
+        listaCategoria.add(cat1);
+        listaCategoria.add(cat2);
+
+        return listaCategoria;
     }
 }
---
+++ src/main/java/com/lucasgv/sellme/domain/Categoria.java
@@ -0,0 +1,16 @@
+package com.lucasgv.sellme.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String nome;
+
+}
